3c5e6fd2f9616823ff65222d3052146f2fd42a57
Tim Kräuter <tim.kraeuterr@gmail.com>
2024-04-10 10:56:27 +0200

    Cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,10 @@ repositories {
 
 dependencies {
     implementation("com.github.ajalt.clikt:clikt:4.2.2")
-    // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     implementation("org.camunda.bpm.model:camunda-bpmn-model:7.20.0")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    // https://mvnrepository.com/artifact/org.camunda.bpm.model/camunda-bpmn-model
 }
 
 tasks.test {
@@ -29,6 +27,7 @@ kotlin {
 
 tasks.shadowJar {
     manifest {
+        // Choose a main class here to build the specific CLI tool.
 //        attributes["Main-Class"] = "parallel.ParallelBranchMainKt"
 //        attributes["Main-Class"] = "blocks.BlockMainKt"
         attributes["Main-Class"] = "statistics.StatsMainKt"
